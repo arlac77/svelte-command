1d7c94814c0aa86c76a129e100c2d0d365855968
Markus Felten <Markus.Felten@gmx.de>
2022-07-13 21:52:57 +0200

    test: with vite@3

--- tests/cafe/interaction.js
+++ tests/cafe/interaction.js
@@ -1,6 +1,6 @@
 import { Selector } from "testcafe";
 
-const base = "http://localhost:3000/services/svelte-command/";
+const base = "http://localhost:5173/examples/svelte-command/";
 
 fixture`command`.page`${base}index.html`;
 
