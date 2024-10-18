cd66c3e858d3ce8e4e8f466e14a027c7cb53ce22
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:39 +0100

    add missing src/components/DateTime.svelte from template

---
+++ src/components/DateTime.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { dateFormatter } from "../util.mjs";
+  let { date } = $props();
+</script>
+
+{#if date}
+  <time datetime={date.toISOString()}>{dateFormatter.format(date)}</time>
+{/if}
