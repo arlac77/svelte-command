c876a0f8bede1abe1183903d243b89a5914c3e6d
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:41 +0100

    add missing src/components/Duration.svelte from template

---
+++ src/components/Duration.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { formatDuration, formatDurationISO } from "../util.mjs";
+  let { seconds } = $props();
+</script>
+
+<time datetime={formatDurationISO(seconds)}>{formatDuration(seconds)}</time>
