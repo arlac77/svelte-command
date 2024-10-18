d08d4c28525a6c99118d1abe6d28319f7338b207
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:50 +0100

    add missing src/components/ServiceWorkerDetails.svelte from template

---
+++ src/components/ServiceWorkerDetails.svelte
@@ -0,0 +1,17 @@
+<script>
+  let { serviceWorker } = $props();
+</script>
+
+<tr>
+  <td colspan="3">Service Worker</td>
+</tr>
+<tr>
+  <td></td>
+  <td>State</td>
+  <td>{serviceWorker.state}</td>
+</tr>
+<tr>
+  <td></td>
+  <td>scriptURL</td>
+  <td>{serviceWorker.scriptURL}</td>
+</tr>
