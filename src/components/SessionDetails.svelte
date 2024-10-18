7612dd551b59764277a7f3b0e6d02e1e5168b894
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:53 +0100

    add missing src/components/SessionDetails.svelte from template

---
+++ src/components/SessionDetails.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { EntitlementBadges, Entitlement } from "svelte-entitlement";
+  import DateTime from "./DateTime.svelte";
+  let { session } = $props();
+</script>
+
+<tr>
+  <td colspan="3">Session</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Username</td>
+  <td>{session.username || ""}</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Expiration</td>
+  <td class={session.isValid ? "ok" : "error"}>
+    <DateTime date={session.expirationDate} />
+  </td>
+</tr>
+<tr>
+  <td></td>
+  <td>Entitlements</td>
+  <td>
+    <EntitlementBadges
+      entitlements={[...session.entitlements]
+        .sort()
+        .map(name => new Entitlement(name))}
+    />
+  </td>
+</tr>
