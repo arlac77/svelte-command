35fd58d56fc53eefd64618eb43eab5b2287c27e9
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:44 +0100

    add missing src/components/Modal.svelte from template

---
+++ src/components/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+  let { close } = $props();
+
+  let background;
+
+  const handleKeyup = event => {
+    if (event.key === "Escape") {
+      event.preventDefault();
+      close();
+    }
+  };
+
+  const handleOuterClick = event => {
+    if (event.target === background) {
+      event.preventDefault();
+      close();
+    }
+  };
+</script>
+
+<svelte:window keyup={handleKeyup} />
+
+<div
+  role="none"
+  click={handleOuterClick}
+  keyup={handleKeyup}
+  bind:this={background}
+>
+  <div class="center modal">
+    <div class="window">
+      <slot />
+    </div>
+  </div>
+</div>
