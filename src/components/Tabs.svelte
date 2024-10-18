555b747537fc70f46056b7aeb17a29fd5b77b0a0
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:58 +0100

    add missing src/components/Tabs.svelte from template

---
+++ src/components/Tabs.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { setContext } from "svelte";
+
+  const tabs = { current: "t5", tab: {} };
+
+  setContext("TABS", tabs);
+</script>
+
+<slot />
+
+<!---
+{#each Object.entries(tabs.tab) as tab}
+  {JSON.stringify(tab)}
+  <svelte:component this={tab[1].header} />
+{/each}
+-->
