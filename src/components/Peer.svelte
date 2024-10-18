2744eb9c30bb8e0eb2d225a03e8b2c654b115773
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:46 +0100

    add missing src/components/Peer.svelte from template

---
+++ src/components/Peer.svelte
@@ -0,0 +1,16 @@
+<script>
+  import TCPSocket from "./TCPSocket.svelte";
+  let { peer } = $props();
+</script>
+
+<div class="peer">
+  <TCPSocket socket={peer} />
+  {#if peer.referrer}
+    (referrer
+    <TCPSocket socket={peer.referrer} />)
+  {/if}
+  {#if peer.to}
+    (to
+    <TCPSocket socket={peer.to} />)
+  {/if}
+</div>
