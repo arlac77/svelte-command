3e2ff9d2071b66c2493ad42fd029c1e8620ac49c
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:37 +0100

    add missing src/components/Collapse.svelte from template

---
+++ src/components/Collapse.svelte
@@ -0,0 +1,15 @@
+<script>
+  let open = $state(false);
+
+  function onclick(event) {
+    event.preventDefault();
+    open = !open;
+  }
+</script>
+
+<button {onclick}>
+  <slot />
+</button>
+{#if open}
+  <slot name="content" />
+{/if}
