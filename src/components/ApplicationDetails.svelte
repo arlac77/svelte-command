76875c90b77ced597684d00a191bf803a680d66e
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:34 +0100

    add missing src/components/ApplicationDetails.svelte from template

---
+++ src/components/ApplicationDetails.svelte
@@ -0,0 +1,15 @@
+<script>
+  let { name, version, description } = $props();
+</script>
+
+<tr>
+  <td colspan="3">{name}</td>
+</tr>
+<tr>
+  <td colspan="3">{description}</td>
+</tr>
+<tr>
+  <td></td>
+  <td>Version</td>
+  <td>{version}</td>
+</tr>
