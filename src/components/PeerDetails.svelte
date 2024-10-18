fd28756198db7333d7eeb19e2678cd59407ed205
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:47 +0100

    add missing src/components/PeerDetails.svelte from template

---
+++ src/components/PeerDetails.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Peer from "./Peer.svelte";
+  let { peers } = $props();
+</script>
+
+<tr>
+  <td>Peers</td>
+  <td colspan="2">
+    {#each peers as peer}
+      <Peer {peer} />
+    {/each}
+  </td>
+</tr>
