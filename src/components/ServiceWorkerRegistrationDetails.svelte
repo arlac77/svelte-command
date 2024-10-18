1a12a89924853f62a8b8efd2cac87e964390b920
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:52 +0100

    add missing src/components/ServiceWorkerRegistrationDetails.svelte from template

---
+++ src/components/ServiceWorkerRegistrationDetails.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Command, CommandButton } from "svelte-command";
+  let { serviceWorkerRegistration } = $props();
+</script>
+
+<tr>
+  <td></td>
+  <td>Scope</td>
+  <td id="serviceWorkerScope">{serviceWorkerRegistration.scope}</td>
+</tr>
+<tr>
+  <td></td>
+  <td colspan="2">
+    <CommandButton
+      command={new Command(() => serviceWorkerRegistration.update(), {
+        title: "Update",
+        shortcuts: "Command+u"
+      })}
+    />
+    <CommandButton
+      command={new Command(() => serviceWorkerRegistration.unregister(), {
+        title: "Unregister"
+      })}
+    />
+  </td>
+</tr>
