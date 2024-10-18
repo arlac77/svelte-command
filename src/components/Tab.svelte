53181ddc2e5f4210d3c00eac432b5ea6043d2e2b
Markus Felten <Markus.Felten@gmx.de>
2024-10-18 14:59:57 +0100

    add missing src/components/Tab.svelte from template

---
+++ src/components/Tab.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { getContext } from "svelte";
+
+  export let id;
+
+  let header, content;
+  const tabs = getContext("TABS");
+  tabs.tab[id] = { header, content };
+</script>
+
+  <div bind:this={header}>
+    <slot name="header" />
+  </div>
+  {#if tabs.current === id}
+  <div bind:this={content}>
+    <slot />
+  </div>
+{/if}
